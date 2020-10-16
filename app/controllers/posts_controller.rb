138f9ff60f950e1a792528c7ba8a27549e94b165
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-10-16 12:52:29 -0400

    Add member to see the author only when sign in function, and log in/ log out buttons

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_member!, except: [:index, :show]
 
   # GET /posts
