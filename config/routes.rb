ed7e260618fb07f059103701fed815f22506651d
hguruman <hguruman@gmail.com>
2020-10-16 19:59:44 +0400

    Fix typo error in the route file

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "post#index"
+  root to: "posts#index"
 end
